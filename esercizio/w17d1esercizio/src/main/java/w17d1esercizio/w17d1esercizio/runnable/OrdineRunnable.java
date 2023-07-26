27f4117e934c87a6fa205a7a05584e4a0be90c7b
Walter Barbieri <walter90@hotmail.it>
2023-07-26 09:28:10 +0200

    ottimizzato ordineRunnable

--- esercizio/w17d1esercizio/src/main/java/w17d1esercizio/w17d1esercizio/runnable/OrdineRunnable.java
+++ esercizio/w17d1esercizio/src/main/java/w17d1esercizio/w17d1esercizio/runnable/OrdineRunnable.java
@@ -36,67 +36,32 @@ public class OrdineRunnable implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		log.info("**********ORDINE " + ordine1.getNumeroOrdine() + "**********");
-		log.info("Coperti = " + ordine1.getNumeroCoperti());
-		log.info("Orario = " + LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute());
-		log.info("Tavolo Numero = " + ordine1.getTavolo().getNumeroTavolo());
-		log.info("");
-		log.info("Ordine = ");
-		ordine1.getPizzaList().forEach(el -> log.info(el.toString()));
-		ordine1.getBevandeList().forEach(el -> log.info(el.toString()));
-		ordine1.getOggettisticaList().forEach(el -> log.info(el.toString()));
-		log.info("");
-		log.info("Coperto segretissimo = " + ordine1.getSecretCoperto() + "€");
-		log.info("Stato Ordine = " + ordine1.getStatoOrdine());
-		log.info("");
-		log.info("Totale = " + ordine1.getTotale() + "€");
-		log.info("");
-		if (ordine1.getNota() == null) {
-			ordine1.setNota("...");
-		}
-		log.info("Note = " + ordine1.getNota());
-		log.info("");
+		printOrdine(ordine1);
+		printOrdine(ordine2);
+		printOrdine(ordine3);
 
-		log.info("**********ORDINE " + ordine2.getNumeroOrdine() + "**********");
-		log.info("Coperti = " + ordine2.getNumeroCoperti());
-		log.info("Orario = " + LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute());
-		log.info("Tavolo Numero = " + ordine2.getTavolo().getNumeroTavolo());
-		log.info("");
-		log.info("Ordine = ");
-		ordine2.getPizzaList().forEach(el -> log.info(el.toString()));
-		ordine2.getBevandeList().forEach(el -> log.info(el.toString()));
-		ordine2.getOggettisticaList().forEach(el -> log.info(el.toString()));
-		log.info("");
-		log.info("Coperto segretissimo = " + ordine2.getSecretCoperto() + "€");
-		log.info("Stato Ordine = " + ordine2.getStatoOrdine());
-		log.info("");
-		log.info("Totale = " + ordine2.getTotale() + "€");
-		log.info("");
-		if (ordine2.getNota() == null) {
-			ordine2.setNota("...");
-		}
-		log.info("Note = " + ordine2.getNota());
-		log.info("");
+	}
 
-		log.info("**********ORDINE " + ordine3.getNumeroOrdine() + "**********");
-		log.info("Coperti = " + ordine3.getNumeroCoperti());
+	public void printOrdine(Ordine ordine) {
+		log.info("**********ORDINE " + ordine.getNumeroOrdine() + "**********");
+		log.info("Coperti = " + ordine.getNumeroCoperti());
 		log.info("Orario = " + LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute());
-		log.info("Tavolo Numero = " + ordine3.getTavolo().getNumeroTavolo());
+		log.info("Tavolo Numero = " + ordine.getTavolo().getNumeroTavolo());
 		log.info("");
 		log.info("Ordine = ");
-		ordine3.getPizzaList().forEach(el -> log.info(el.toString()));
-		ordine3.getBevandeList().forEach(el -> log.info(el.toString()));
-		ordine3.getOggettisticaList().forEach(el -> log.info(el.toString()));
+		ordine.getPizzaList().forEach(el -> log.info(el.toString()));
+		ordine.getBevandeList().forEach(el -> log.info(el.toString()));
+		ordine.getOggettisticaList().forEach(el -> log.info(el.toString()));
 		log.info("");
-		log.info("Coperto segretissimo = " + ordine3.getSecretCoperto() + "€");
-		log.info("Stato Ordine = " + ordine3.getStatoOrdine());
+		log.info("Coperto segretissimo = " + ordine.getSecretCoperto() + "€");
+		log.info("Stato Ordine = " + ordine.getStatoOrdine());
 		log.info("");
-		log.info("Totale = " + ordine3.getTotale() + "€");
+		log.info("Totale = " + ordine.getTotale() + "€");
 		log.info("");
-		if (ordine3.getNota() == null) {
-			ordine3.setNota("...");
+		if (ordine.getNota() == null) {
+			ordine.setNota("...");
 		}
-		log.info("Note = " + ordine3.getNota());
+		log.info("Note = " + ordine.getNota());
 		log.info("");
 	}
 }
